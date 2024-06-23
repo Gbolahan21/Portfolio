dede328bdc0d380587d7e3b09e2d25735c6759d0
gbolahan <mo@carrotcredit.com>
2024-06-23 21:33:45 +0100

    staging

--- src/Signin/index.js
+++ src/Signin/index.js
@@ -5,7 +5,7 @@ import { Section1 } from '../../components';
 const Signin = () => {
     const heading = (
         <div>
-          <h1>to test how to pass props across a component</h1>
+          <h1>to test how to pass props across a component in react</h1>
         </div>
     );
 }
